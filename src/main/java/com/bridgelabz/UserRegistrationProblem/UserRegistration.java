150fe8e8365c1936fe20274241dca9f9996ae3a6
DAXILSAXENA <daxildaxil@gmail.com>
2021-07-04 23:10:44 +0530

    Added function to verify Last Name using REGEX

--- src/main/java/com/bridgelabz/UserRegistrationProblem/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistrationProblem/UserRegistration.java
@@ -15,6 +15,19 @@ public class UserRegistration {
 			System.out.println("It is a valid First Name");
 		else
 			System.out.println("It is an invalid First Name");
+		System.out.println("\n");
+	}
+
+	// UC-2
+	// Function to verify LastName and print whether it is valid or not:
+
+	public void verifyLastName() {
+		System.out.println("Enter Last name:");
+		String lastName = scan.next();
+		if (lastName.matches("^[A-Z]{1}[a-z]{2,}$") == true)
+			System.out.println("It is a valid Last Name");
+		else
+			System.out.println("It is an invalid Last Name");
 	}
 
 	public static void main(String[] args) {
@@ -22,6 +35,7 @@ public class UserRegistration {
 		System.out.println("Welcome to User Registration problem:");
 		UserRegistration object = new UserRegistration();
 		object.verifyFirstName();
+		object.verifyLastName();
 	}
 
 }
