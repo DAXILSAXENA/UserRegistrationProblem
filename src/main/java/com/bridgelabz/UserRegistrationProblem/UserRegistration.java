059d027d50714dd261cd4338c460b58b669e0144
DAXILSAXENA <daxildaxil@gmail.com>
2021-07-04 22:35:19 +0530

    Added Welcome Message

---
+++ src/main/java/com/bridgelabz/UserRegistrationProblem/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.UserRegistrationProblem;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to User Registration problem:");
+	}
+
+}
