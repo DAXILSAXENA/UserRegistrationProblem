031151c20544d5cd47cf0efb61a6a75f30e04102
DAXILSAXENA <daxildaxil@gmail.com>
2021-07-04 23:00:20 +0530

    Added function to verify First Name using REGEX

--- src/main/java/com/bridgelabz/UserRegistrationProblem/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistrationProblem/UserRegistration.java
@@ -1,10 +1,27 @@
 package com.bridgelabz.UserRegistrationProblem;
 
+import java.util.Scanner;
+
 public class UserRegistration {
+	Scanner scan = new Scanner(System.in);
+
+	// UC-1
+	// Function to verify FirstName and print whether it is valid or not:
+
+	public void verifyFirstName() {
+		System.out.println("Enter First name:");
+		String firstName = scan.next();
+		if (firstName.matches("^[A-Z]{1}[a-z]{2,}$") == true)
+			System.out.println("It is a valid First Name");
+		else
+			System.out.println("It is an invalid First Name");
+	}
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to User Registration problem:");
+		UserRegistration object = new UserRegistration();
+		object.verifyFirstName();
 	}
 
 }
